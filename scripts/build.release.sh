0bcb9ac9983b8a2b95f13c858daf48d6d9d73f97
Gyuho Lee <gyuho.lee@avalabs.org>
2022-04-25 12:41:57 -0700

    *: initial commit
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

---
+++ scripts/build.release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xue
+
+if ! [[ "$0" =~ scripts/build.release.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+# git submodule add https://github.com/googleapis/googleapis
+# git submodule add https://github.com/ava-labs/avalanche-network-runner
+git submodule update --init --remote
+
+cargo build --release
