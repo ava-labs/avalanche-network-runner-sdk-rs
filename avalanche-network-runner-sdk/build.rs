96ed12268bc7082b42d0e27aac83380533badee7
Sam Batschelet <sam.batschelet@avalabs.org>
2022-05-07 20:11:17 -0400

    protoc: pass --experimental_allow_proto3_optional to tonic build
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- avalanche-network-runner-sdk/build.rs
+++ avalanche-network-runner-sdk/build.rs
@@ -3,6 +3,7 @@ fn main() {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
             &[
                 "googleapis/google/pubsub/v1/pubsub.proto",
