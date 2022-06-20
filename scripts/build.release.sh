cd166e0f90f8d2e20aff6af74c8a74d29b027bb1
Gyuho Lee <gyuho.lee@avalabs.org>
2022-06-20 21:51:25 +0800

    sdk: remove submodule dependencies for proto
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- scripts/build.release.sh
+++ scripts/build.release.sh
@@ -6,8 +6,4 @@ if ! [[ "$0" =~ scripts/build.release.sh ]]; then
   exit 255
 fi
 
-# git submodule add https://github.com/googleapis/googleapis
-# git submodule add https://github.com/ava-labs/avalanche-network-runner
-git submodule update --init
-
 cargo build --release
--- scripts/tests.unused.sh
+++ scripts/tests.unused.sh
@@ -6,8 +6,6 @@ if ! [[ "$0" =~ scripts/tests.unused.sh ]]; then
   exit 255
 fi
 
-git submodule update --init
-
 # https://github.com/est31/cargo-udeps
 cargo install cargo-udeps --locked
 cargo +nightly udeps
