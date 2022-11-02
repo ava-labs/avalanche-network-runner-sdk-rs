39711c26dd5f3b2459bb311f6a24a653b6bd0f4f
Gyuho Lee <gyuho.lee@avalabs.org>
2022-11-02 14:44:30 -0700

    *: sync with ANR 1.3.0, use avago 1.9.1 (#15)
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>
    
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- avalanche-network-runner-sdk/examples/status.rs
+++ avalanche-network-runner-sdk/examples/status.rs
@@ -1,6 +1,5 @@
 use std::env::args;
 
-use log::info;
 use tokio::runtime::Runtime;
 
 use avalanche_network_runner_sdk::Client;
@@ -16,9 +15,9 @@ fn main() {
     let url = args().nth(1).expect("no url given");
     let rt = Runtime::new().unwrap();
 
-    info!("creating client");
+    log::info!("creating client");
     let cli = rt.block_on(Client::new(&url));
 
     let resp = rt.block_on(cli.status()).expect("failed status");
-    info!("status response: {:?}", resp);
+    log::info!("status response: {:?}", resp);
 }
--- scripts/tests.e2e.sh
+++ scripts/tests.e2e.sh
@@ -51,7 +51,7 @@ AVALANCHEGO_PLUGIN_DIR=/tmp/avalanchego-v${AVALANCHEGO_VERSION}/plugins
 # download avalanche-network-runner
 # https://github.com/ava-labs/avalanche-network-runner
 # TODO: use "go install -v github.com/ava-labs/avalanche-network-runner/cmd/avalanche-network-runner@v${NETWORK_RUNNER_VERSION}"
-NETWORK_RUNNER_VERSION=1.2.2
+NETWORK_RUNNER_VERSION=1.3.0
 DOWNLOAD_PATH=/tmp/avalanche-network-runner.tar.gz
 DOWNLOAD_URL=https://github.com/ava-labs/avalanche-network-runner/releases/download/v${NETWORK_RUNNER_VERSION}/avalanche-network-runner_${NETWORK_RUNNER_VERSION}_linux_amd64.tar.gz
 if [[ ${GOOS} == "darwin" ]]; then
