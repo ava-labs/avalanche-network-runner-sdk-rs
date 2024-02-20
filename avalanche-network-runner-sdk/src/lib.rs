2583d594883822d53e6b9d9175a630144ec098a1
Richard Melkonian <35300528+0xmovses@users.noreply.github.com>
2024-02-20 22:49:35 +0000

    Add support for missing RPC calls (#24)
    * add-remove nodes to client
    
    * add validator methods
    
    * add vm_id
    
    * add more methods
    
    * add validators spec
    
    * add missing rpc methods
    
    * add doc comments

--- avalanche-network-runner-sdk/src/lib.rs
+++ avalanche-network-runner-sdk/src/lib.rs
@@ -4,6 +4,11 @@ use std::{
 };
 
 use log::info;
+use rpcpb::{
+    AddPermissionlessValidatorRequest, AddPermissionlessValidatorResponse,
+    AddSubnetValidatorsRequest, AddSubnetValidatorsResponse, RemoveSubnetValidatorsRequest,
+    RemoveSubnetValidatorsResponse,
+};
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
 use tonic::transport::Channel;
@@ -13,8 +18,11 @@ pub mod rpcpb {
 }
 pub use rpcpb::{
     control_service_client::ControlServiceClient, ping_service_client::PingServiceClient,
-    BlockchainSpec, HealthRequest, HealthResponse, PingRequest, PingResponse, StartRequest,
-    StartResponse, StatusRequest, StatusResponse, StopRequest, StopResponse, UrIsRequest,
+    AddNodeRequest, AddNodeResponse, BlockchainSpec, HealthRequest, HealthResponse,
+    ListBlockchainsRequest, ListBlockchainsResponse, ListSubnetsRequest, ListSubnetsResponse,
+    PingRequest, PingResponse, RemoveNodeRequest, RemoveNodeResponse, StartRequest, StartResponse,
+    StatusRequest, StatusResponse, StopRequest, StopResponse, UrIsRequest, VmidRequest,
+    VmidResponse,
 };
 
 pub struct Client<T> {
@@ -131,6 +139,125 @@ impl Client<Channel> {
 
         let stop_resp = resp.into_inner();
         Ok(stop_resp)
+    }
+
+    /// Add a node to the currently running cluster.
+    pub async fn add_node(&self, req: AddNodeRequest) -> io::Result<AddNodeResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .add_node(req)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, format!("failed add_node '{}'", e)))?;
+
+        let add_node_resp = resp.into_inner();
+        Ok(add_node_resp)
+    }
+
+    /// Remove a node from the currently running cluster.
+    pub async fn remove_node(&self, req: RemoveNodeRequest) -> io::Result<RemoveNodeResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .remove_node(req)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, format!("failed remove_node '{}'", e)))?;
+
+        let remove_node_resp = resp.into_inner();
+        Ok(remove_node_resp)
+    }
+
+    /// Adds a new permissionless validator to the network.
+    pub async fn add_permissionless_validator(
+        &self,
+        req: AddPermissionlessValidatorRequest,
+    ) -> io::Result<AddPermissionlessValidatorResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .add_permissionless_validator(req)
+            .await
+            .map_err(|e| {
+                Error::new(
+                    ErrorKind::Other,
+                    format!("failed add_permissionless_validator '{}'", e),
+                )
+            })?;
+        let resp = resp.into_inner();
+        Ok(resp)
+    }
+
+    /// Adds a new permissionless validator to the network.
+    pub async fn add_validator(
+        &self,
+        req: AddSubnetValidatorsRequest,
+    ) -> io::Result<AddSubnetValidatorsResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .add_subnet_validators(req)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, format!("failed add_validators '{}'", e)))?;
+        let resp = resp.into_inner();
+        Ok(resp)
+    }
+
+    /// Removes a validator from the network.
+    pub async fn remove_validator(
+        &self,
+        req: RemoveSubnetValidatorsRequest,
+    ) -> io::Result<RemoveSubnetValidatorsResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .remove_subnet_validator(req)
+            .await
+            .map_err(|e| {
+                Error::new(
+                    ErrorKind::Other,
+                    format!("failed remove_validators '{}'", e),
+                )
+            })?;
+        let resp = resp.into_inner();
+        Ok(resp)
+    }
+
+    /// Fetches the VM ID for the current cluster.
+    pub async fn vm_id(&self, req: VmidRequest) -> io::Result<VmidResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .vmid(req)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, format!("failed vm_id '{}'", e)))?;
+        let resp = resp.into_inner();
+        Ok(resp)
+    }
+
+    /// Fetches the list of blockchains for the current cluster.
+    pub async fn list_blockchains(
+        &self,
+        req: ListBlockchainsRequest,
+    ) -> io::Result<ListBlockchainsResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client.list_blockchains(req).await.map_err(|e| {
+            Error::new(ErrorKind::Other, format!("failed list_blockchains '{}'", e))
+        })?;
+        let resp = resp.into_inner();
+        Ok(resp)
+    }
+
+    /// Fetches the list of subnets for the current cluster.
+    pub async fn list_subnets(&self, req: ListSubnetsRequest) -> io::Result<ListSubnetsResponse> {
+        let mut control_client = self.grpc_client.control_client.lock().await;
+        let req = tonic::Request::new(req);
+        let resp = control_client
+            .list_subnets(req)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, format!("failed list_subnets '{}'", e)))?;
+        let resp = resp.into_inner();
+        Ok(resp)
     }
 }
 
